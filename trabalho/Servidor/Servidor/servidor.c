e82b197e14f7931823f0d2e71391e8df2dc5e409
ggeraldes <84858909+ggeraldes@users.noreply.github.com>
2023-06-12 18:40:56 +0100

    Add files via upload

--- trabalho/Servidor/Servidor/servidor.c
+++ trabalho/Servidor/Servidor/servidor.c
@@ -1,8 +1,8 @@
-ï»¿
-#include "../../header.h"
+#include "../header.h"
 
 
-
+COORD geral;
+CONSOLE_SCREEN_BUFFER_INFO csbi;
 
 typedef struct {
 	int posX, posY; //posicao
@@ -17,65 +17,72 @@ DWORD WINAPI ChangeTab(LPVOID param) {
 	srand((unsigned)time(0) + GetCurrentThreadId());
 	faixa->stop = FALSE;
 	faixa->nCars = 0;
-	
+
 	//_tprintf(TEXT("\nSTATE:%d\n"), faixa->state);
-	while (game.state!=3) {
+	while (game.state != 3) {
 		for (int i = 0; i < COLUNAS; i++)
 			if (faixa->col[i].val == 2)
 				faixa->nCars++;
-			
+
 		WaitForSingleObject(game.gameMutex, INFINITE);
-			if (faixa->state == 1) {//normal
-				for (int i = COLUNAS - 1; i > 0; i--) {
-					if (faixa->col[i].val != 1 && faixa->col[i].val != 3 && faixa->col[i-1].val != 3 && faixa->col[i].val != 2) {
-								faixa->col[i].val = faixa->col[i - 1].val;
-								faixa->col[i - 1].val = 0;
-							
-					}
-					if(faixa->col[i-1].val == 2 && i== COLUNAS - 1)
-						faixa->col[i].val = 0;
+		if (faixa->state == 1) {//normal
+			for (int i = COLUNAS - 1; i > 0; i--) {
+
+				if (faixa->col[i].val == 2 && i == COLUNAS - 1)
+					faixa->col[i].val = 0;
+
+
+				if (faixa->col[i].val != 1 && faixa->col[i].val != 3 && faixa->col[i - 1].val != 3 && faixa->col[i].val != 2) {
+					faixa->col[i].val = faixa->col[i - 1].val;
+					faixa->col[i - 1].val = 0;
+
 				}
-				if (random_60_40() == 2 && faixa->nCars<MAXCARROS)
-					if (faixa->col[0].val != 3)
+				
+			}
+			if (random_60_40() == 2 && faixa->nCars < MAXCARROS)
+				if (faixa->col[0].val != 3)
 					faixa->col[0].val = 2;
-					
 
-				//_tprintf(TEXT("\nCARS:%d\n"), faixa->nCars);
-			}
-			else if (faixa->state == 2) {//inverso
-				for (int i = 0; i < COLUNAS; i++) {
-					if (faixa->col[i].val != 1 && faixa->col[i].val != 3 && faixa->col[i + 1].val != 3 && faixa->col[i].val != 2) {
-							faixa->col[i].val = faixa->col[i + 1].val;
-							faixa->col[i + 1].val = 0;
-						
-					}
-					if (faixa->col[i+1].val == 2 && i == 0)
-						faixa->col[i].val = 0;
+
+			//_tprintf(TEXT("\nCARS:%d\n"), faixa->nCars);
+		}
+		else if (faixa->state == 2) {//inverso
+			for (int i = 0; i < COLUNAS; i++) {
+
+				if (faixa->col[i].val == 2 && i == 0)
+					faixa->col[i].val = 0;
+
+				if (faixa->col[i].val != 1 && faixa->col[i].val != 3 && faixa->col[i + 1].val != 3 && faixa->col[i].val != 2) {
+					faixa->col[i].val = faixa->col[i + 1].val;
+					faixa->col[i + 1].val = 0;
+
 				}
-				if (random_60_40() == 2 && faixa->nCars < MAXCARROS)
-					if(faixa->col[COLUNAS - 1].val != 3)
-						faixa->col[COLUNAS - 1].val = 2;
-
-
+				
 			}
-		
-		
-		
-		
+			if (random_60_40() == 2 && faixa->nCars < MAXCARROS)
+				if (faixa->col[COLUNAS - 1].val != 3)
+					faixa->col[COLUNAS - 1].val = 2;
+
+
+		}
+
+
+
+
 		//x++;
 		faixa->nCars = 0;
 		ReleaseMutex(game.gameMutex);
 		if (faixa->stop) {
 			Sleep(faixa->stopTime * 1000);
 			faixa->stop = FALSE;
-		}	
+		}
 		else
-		Sleep(faixa->velocCarros*200);
-		
+			Sleep(faixa->velocCarros * 200);
+
 
 	}
 	//_tprintf(TEXT("C%d consumiu %d items.\n"), dados->id, soma);
-	
+
 	return 0;
 }
 //------------------------------------------ BUFFER CIRCULAR -------------------------------------------------------------
@@ -85,16 +92,17 @@ DWORD WINAPI ThreadServidor(LPVOID param) {
 	//int contador = 0;
 	//int soma = 0;
 
-	COORD t;
-	t.X = 0;
-	t.Y = 25;
+	COORD t, comands;
+	t.X = 0; comands.X = 26;
+	//t.Y = 25; 
+	comands.Y = 1;
 	//DWORD size;
 
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 	while (!(dados->terminar)) {
 		//FillConsoleOutputCharacter(hConsole, _T(' '), 80 * 1, t, &size);
-		SetConsoleCursorPosition(hConsole, t);
+		//SetConsoleCursorPosition(hConsole, t);
 		//aqui entramos na logica da aula teorica
 
 		//esperamos por uma posicao para lermos
@@ -118,32 +126,47 @@ DWORD WINAPI ThreadServidor(LPVOID param) {
 		//libertamos o semaforo. temos de libertar uma posicao de escrita
 		ReleaseSemaphore(dados->hSemEscrita, 1, NULL);
 
+		GetConsoleScreenBufferInfo(hConsole, &csbi);
 		//contador++;
 		//soma += cel.val;
-		
-		_tprintf(TEXT("O%d comando: %d, %d.\n"), cel.id, cel.comando[0], cel.comando[1]);
-		t.X++;
+
+		//_tprintf(TEXT("O%d comando: %d, %d.\n"), cel.id, cel.comando[0], cel.comando[1]);
+		//t.X++;
 		if (cel.comando[0] == 1) {
 			for (int i = 0; i < game.nFaixas; i++) { //parar por x tempo
-				game.faixa[i].stop=TRUE;
+				game.faixa[i].stop = TRUE;
 				game.faixa[i].stopTime = cel.comando[1];
-				
+
 			}
-				
+			SetConsoleCursorPosition(hConsole, comands);
+			_tprintf(TEXT("O%d: stop %d segundos\n"),cel.id, cel.comando[1]);
+			comands.Y++;
+			SetConsoleCursorPosition(hConsole, geral);
+
 		}
 		else if (cel.comando[0] == 2) { //inverter sentido
-			if(game.faixa[cel.comando[1]].state==1)
+			if (game.faixa[cel.comando[1]].state == 1)
 				game.faixa[cel.comando[1]].state = 2;
 			else
 				game.faixa[cel.comando[1]].state = 1;
 
-		}
-		else if (cel.comando[0] == 3) { //inserir obstaculos
-				game.faixa[cel.comando[1]].col[cel.comando[2]].val = 3;
+			SetConsoleCursorPosition(hConsole, comands);
+			_tprintf(TEXT("O%d: invert faixa %d\n"),cel.id, cel.comando[1]);
+			comands.Y++;
+			SetConsoleCursorPosition(hConsole, geral);
 
 		}
-			
-			
+		else if (cel.comando[0] == 3) { //inserir obstaculos
+			game.faixa[cel.comando[1]].col[cel.comando[2]].val = 3;
+
+			SetConsoleCursorPosition(hConsole, comands);
+			_tprintf(TEXT("O%d: insert faixa %d, coluna %d\n"), cel.id, cel.comando[1], cel.comando[2]);
+			comands.Y++;
+			SetConsoleCursorPosition(hConsole, geral);
+
+		}
+
+
 	}
 	//_tprintf(TEXT("C%d consumiu %d items.\n"), dados->id, soma);
 
@@ -160,48 +183,51 @@ DWORD WINAPI EnviaTabuleiro(LPVOID param) {
 	t.X = 0; y.X = 9;
 	t.Y = 0; y.Y = 12;
 	DWORD size;
-		
+
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 	while (!(dados->terminar)) {
-		
-			FillConsoleOutputCharacter(hConsole, _T(' '), 80 * 1, t, &size);
-			SetConsoleCursorPosition(hConsole,t);
 
-			_tprintf(TEXT("  "));
-			for(int i=0;i<10;i++)
-				_tprintf(TEXT("%d"), i);
-			for (int i = 0; i < 10; i++)
-				_tprintf(TEXT("%d"), i);
-			_tprintf(TEXT("\n"));
+		GetConsoleScreenBufferInfo(hConsole, &csbi);
 
-			for (int i = 0; i < game.nFaixas; i++) {
-				_tprintf(TEXT("%d|"), i);
-				for (int y = 0; y < COLUNAS; y++)
-					if (game.faixa[i].col[y].val == 1) {
-						SetConsoleTextAttribute(hConsole, (FOREGROUND_GREEN));
-						_tprintf(TEXT("s"));
-						SetConsoleTextAttribute(hConsole, (FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED));
-					}
-					else if (game.faixa[i].col[y].val == 2) {
-						SetConsoleTextAttribute(hConsole, (FOREGROUND_RED));
-						_tprintf(TEXT("c"));
-						SetConsoleTextAttribute(hConsole, (FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED));
-					}
-					else if (game.faixa[i].col[y].val == 3) {
-						SetConsoleTextAttribute(hConsole, (FOREGROUND_BLUE));
-						_tprintf(TEXT("B"));
-						SetConsoleTextAttribute(hConsole, (FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED));
-					}
-					else
-						_tprintf(TEXT(" "));
-				_tprintf(TEXT("|\n"));
+		FillConsoleOutputCharacter(hConsole, _T(' '), 80 * 1, t, &size);
+		SetConsoleCursorPosition(hConsole, t);
 
-			}
-			SetConsoleCursorPosition(hConsole, y);
-			//Sleep(1000);
+		_tprintf(TEXT("  "));
+		for (int i = 0; i < 10; i++)
+			_tprintf(TEXT("%d"), i);
+		for (int i = 0; i < 10; i++)
+			_tprintf(TEXT("%d"), i);
+		_tprintf(TEXT("\n"));
 
-		//faÃ§o lock ao mutex
+		for (int i = 0; i < game.nFaixas; i++) {
+			_tprintf(TEXT("%d|"), i);
+			for (int y = 0; y < COLUNAS; y++)
+				if (game.faixa[i].col[y].val == 1) {
+					SetConsoleTextAttribute(hConsole, (FOREGROUND_GREEN));
+					_tprintf(TEXT("s"));
+					SetConsoleTextAttribute(hConsole, (FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED));
+				}
+				else if (game.faixa[i].col[y].val == 2) {
+					SetConsoleTextAttribute(hConsole, (FOREGROUND_RED));
+					_tprintf(TEXT("c"));
+					SetConsoleTextAttribute(hConsole, (FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED));
+				}
+				else if (game.faixa[i].col[y].val == 3) {
+					SetConsoleTextAttribute(hConsole, (FOREGROUND_BLUE));
+					_tprintf(TEXT("B"));
+					SetConsoleTextAttribute(hConsole, (FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED));
+				}
+				else
+					_tprintf(TEXT(" "));
+			_tprintf(TEXT("|\n"));
+
+		}
+		geral = csbi.dwCursorPosition;
+		SetConsoleCursorPosition(hConsole, geral);
+		//Sleep(1000);
+
+	//faço lock ao mutex
 		WaitForSingleObject(dados->hMutex, INFINITE);
 
 		//limpa memoria antes de fazer a copia
@@ -217,7 +243,7 @@ DWORD WINAPI EnviaTabuleiro(LPVOID param) {
 		SetEvent(dados->hEvent);
 		Sleep(game.velocCarros * 200);
 
-		ResetEvent(dados->hEvent); //torna o evento novamente nÃ£o assinalado
+		ResetEvent(dados->hEvent); //torna o evento novamente não assinalado
 	}
 
 	return 0;
@@ -225,11 +251,11 @@ DWORD WINAPI EnviaTabuleiro(LPVOID param) {
 //-------------------------------------------------------------------------------------
 
 int _tmain(int argc, LPTSTR argv[]) {
-	
+
 	//DadosThreads dados;
-	
+
 	srand((unsigned)time(0) + GetCurrentThreadId());
-	
+
 
 	TCHAR COM[50]; //COMANDOS
 	//GameState gamestate;
@@ -259,7 +285,7 @@ int _tmain(int argc, LPTSTR argv[]) {
 	//se devolver NULL nao existe e vamos fazer a inicializacao
 
 	if (OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, TEXT("SO2_MEM_PARTILHADA"))) {
-		_tprintf(TEXT("Servidor jÃ¡ se encontra aberto\n"));
+		_tprintf(TEXT("Servidor já se encontra aberto\n"));
 		return -1;
 	}
 
@@ -279,17 +305,17 @@ int _tmain(int argc, LPTSTR argv[]) {
 	}
 	//-----------------------------------------------------------------------------------------------------------
 
-	do{
-		_tprintf(TEXT("Fazer especificaÃ§Ã£o inicial? "));
+	do {
+		_tprintf(TEXT("Fazer especificação inicial? "));
 		_fgetts(COM, 50, stdin);
 		COM[_tcslen(COM) - 1] = '\0';
-		
-			
+
+
 	} while (_tcscmp(COM, TEXT("yes")) != 0 && _tcscmp(COM, TEXT("no")) != 0);
 
 	if (_tcscmp(COM, TEXT("yes")) == 0) {
 		do {
-			_tprintf(TEXT("NÃºmero de faixas: "));
+			_tprintf(TEXT("Número de faixas: "));
 			_tscanf_s(TEXT("%d"), &game.nFaixas);
 		} while (game.nFaixas > 10 || game.nFaixas < 3);
 
@@ -297,10 +323,10 @@ int _tmain(int argc, LPTSTR argv[]) {
 		_tscanf_s(TEXT("%d"), &game.velocCarros);
 		//fflush(stdin);
 	}
-		
+
 	//-----------------------------------------------------------------------------------REGISTRY---------------------------------------------------------------------
 	//Criar ou abrir uma chave do Registry
-	if (RegCreateKeyEx(HKEY_CURRENT_USER,key_name,0,NULL,REG_OPTION_NON_VOLATILE,KEY_ALL_ACCESS,NULL,&key,&result) != ERROR_SUCCESS) {
+	if (RegCreateKeyEx(HKEY_CURRENT_USER, key_name, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &key, &result) != ERROR_SUCCESS) {
 		_tprintf(TEXT("Chave nao foi criada nem aberta ! ERRO!"));
 		return -1;
 	}
@@ -308,26 +334,26 @@ int _tmain(int argc, LPTSTR argv[]) {
 	if (result == REG_CREATED_NEW_KEY) {
 
 		_tprintf(TEXT("A chave foi criada: %s"), key_name);
-		
+
 		if (game.velocCarros == 0 && game.nFaixas == 0) {
 			do {
-				_tprintf(TEXT("\nNÃºmero de faixas: "));
+				_tprintf(TEXT("\nNúmero de faixas: "));
 				_tscanf_s(TEXT("%d"), &game.nFaixas);
-			} while (game.nFaixas > 10 && game.nFaixas<3);
+			} while (game.nFaixas > 10 && game.nFaixas < 3);
 
 			_tprintf(TEXT("\nVelocidade Inicial carros: "));
 			_tcscanf_s(TEXT("%d"), &game.velocCarros);
 		}
 
-		if(RegSetValueEx(key, TEXT("NFaixas"), 0, REG_DWORD, (LPBYTE)&game.nFaixas, sizeof(game.nFaixas)) == ERROR_SUCCESS)
+		if (RegSetValueEx(key, TEXT("NFaixas"), 0, REG_DWORD, (LPBYTE)&game.nFaixas, sizeof(game.nFaixas)) == ERROR_SUCCESS)
 			_tprintf(TEXT("\nNamed value 'NFaixas' was created"));
 
-		if(RegSetValueEx(key, TEXT("VCarrosI"), 0, REG_DWORD, (LPBYTE)&game.velocCarros, sizeof(game.velocCarros)) == ERROR_SUCCESS)
+		if (RegSetValueEx(key, TEXT("VCarrosI"), 0, REG_DWORD, (LPBYTE)&game.velocCarros, sizeof(game.velocCarros)) == ERROR_SUCCESS)
 			_tprintf(TEXT("\nNamed value 'VCarrosI' was created"));
 
-		
+
 	}
-		
+
 	else {
 		_tprintf(TEXT("A chave foi aberta:%s\n"), key_name);
 
@@ -348,11 +374,11 @@ int _tmain(int argc, LPTSTR argv[]) {
 			if (RegQueryValueEx(key, TEXT("VCarrosI"), NULL, NULL, (LPBYTE)&game.velocCarros, &size) == ERROR_SUCCESS) {
 				_tprintf(TEXT("\nVCarrosI: %d"), game.velocCarros);
 			}
-			
-			
+
+
 		}
 	}
-		
+
 	RegCloseKey(key);
 	for (int i = 0; i < game.nFaixas; i++) {
 		game.faixa[i].velocCarros = game.velocCarros;
@@ -360,13 +386,13 @@ int _tmain(int argc, LPTSTR argv[]) {
 		game.faixa[i].state = random_0_1_2();
 		//_tprintf(TEXT("\nyooooooooooo1:%d\n"), game.faixa[i].state);
 	}
-		
+
 	Sleep(2000); //EM PROGRESSO <-----------------------------
 
 	//------------------------------------------------------------------BUFFER CIRCULAR-----------------------------------------------------------------------
 
 	DadosThreads dados;
-	
+
 
 
 	//criar semaforo que conta as escritas
@@ -374,7 +400,7 @@ int _tmain(int argc, LPTSTR argv[]) {
 
 
 	//criar semaforo que conta as leituras
-	//0 porque nao ha nada para ser lido e depois podemos ir atÃ© um maximo de 10 posicoes para serem lidas
+	//0 porque nao ha nada para ser lido e depois podemos ir até um maximo de 10 posicoes para serem lidas
 	dados.hSemLeitura = CreateSemaphore(NULL, 0, TAM_BUFFER, TEXT("SO2_SEMAFORO_LEITURA"));
 
 	//criar mutex para os SERVIDORES
@@ -384,9 +410,9 @@ int _tmain(int argc, LPTSTR argv[]) {
 		_tprintf(TEXT("Erro no CreateSemaphore\n"));
 		return -1;
 	}
-	
 
-	//mapeamos o bloco de memoria para o espaco de enderaÃ§amento do nosso processo
+
+	//mapeamos o bloco de memoria para o espaco de enderaçamento do nosso processo
 	dados.memPar = (BufferCircular*)MapViewOfFile(hFileMapBC, FILE_MAP_ALL_ACCESS, 0, 0, 0);
 
 	if (dados.memPar == NULL) {
@@ -402,7 +428,7 @@ int _tmain(int argc, LPTSTR argv[]) {
 
 	dados.terminar = 0;
 
-	
+
 
 	//lancamos a thread
 	hThread[0] = CreateThread(NULL, 0, ThreadServidor, &dados, 0, NULL);
@@ -411,12 +437,12 @@ int _tmain(int argc, LPTSTR argv[]) {
 		_tprintf(TEXT("Problemas com a thread do buffer circular ...\n"));
 	else
 		_tprintf(TEXT("thread buffer circular inicializada ...\n"));
-	
-	
+
+
 
 
 	//---------------------------------------------------------------------------TABULEIRO-----------------------------------------------------------------------
-	
+
 	//---------------------- MEMORIA PARTILHADA ------------------------------
 	HANDLE hFileMap;
 	ThreadTab tab;
@@ -431,16 +457,16 @@ int _tmain(int argc, LPTSTR argv[]) {
 		PAGE_READWRITE,
 		0,
 		sizeof(GameInfo), // alterar o tamanho do filemapping
-		TEXT("TP_MEM_OPERADOR")); //nome do file mapping, tem de ser Ãºnico
+		TEXT("TP_MEM_OPERADOR")); //nome do file mapping, tem de ser único
 
 	if (hFileMap == NULL) {
 		_tprintf(TEXT("Erro no CreateFileMapping\n"));
-		//CloseHandle(hFile); //recebe um handle e fecha esse handle , no entanto o handle Ã© limpo sempre que o processo termina
+		//CloseHandle(hFile); //recebe um handle e fecha esse handle , no entanto o handle é limpo sempre que o processo termina
 		return 1;
 	}
 
-	//mapeia bloco de memoria para espaÃ§o de endereÃ§amento
-	tab.fileViewMap= (GameInfo*)MapViewOfFile(
+	//mapeia bloco de memoria para espaço de endereçamento
+	tab.fileViewMap = (GameInfo*)MapViewOfFile(
 		hFileMap,
 		FILE_MAP_ALL_ACCESS,
 		0,
@@ -478,7 +504,7 @@ int _tmain(int argc, LPTSTR argv[]) {
 
 	tab.terminar = 0;
 
-	
+
 
 	//--------------------------------
 
@@ -502,26 +528,26 @@ int _tmain(int argc, LPTSTR argv[]) {
 
 	// Set the console cursor position to the top left corner
 	SetConsoleCursorPosition(hConsole, t);
-	
-	
-	
+
+
+
 	FillConsoleOutputCharacter(hConsole, _T(' '), 1, t, &size);
 
 	game.gameMutex = CreateMutex(NULL, FALSE, TEXT("GAME_MUTEX"));
 
 	int random_number = rand() % 20;
-	game.faixa[game.nFaixas-1].col[random_number].val = 1;
+	game.faixa[game.nFaixas - 1].col[random_number].val = 1;
 
 	do {
 		random_number = rand() % 20;
 	} while (game.faixa[game.nFaixas - 1].col[random_number].val == 1);
 
-	game.faixa[game.nFaixas-1].col[random_number].val = 1;
+	game.faixa[game.nFaixas - 1].col[random_number].val = 1;
 
-	
+
 	HANDLE ThreadChangeTab[10];
 
-	for (int i = 1; i < game.nFaixas-1; i++) {
+	for (int i = 1; i < game.nFaixas - 1; i++) {
 		ThreadChangeTab[i] = CreateThread(NULL, 0, ChangeTab, &game.faixa[i], 0, NULL);
 
 		if (ThreadChangeTab[i] == NULL)
@@ -532,34 +558,34 @@ int _tmain(int argc, LPTSTR argv[]) {
 
 	if (hThread[1] == NULL)
 		_tprintf(TEXT("Problemas com a thread da tabela ...\n"));
-	
-	
 
-	
+
+
+
 	//Sleep(2000);
-	
+
 
 	//-----------------------------------------------------------------------------------------------------------------------------------------------------------
-	
+
 	COORD pos;
 	pos.X = 0;
 	pos.Y = 12;
-	
+
 
 	do {
-		
+
 
 		FillConsoleOutputCharacter(hConsole, _T(' '), 80 * 1, pos, &size);
 		SetConsoleCursorPosition(hConsole, pos);
 
-		
-		
+
+
 		_tprintf(TEXT("COMANDO: "));
 		_fgetts(COM, 50, stdin);
-		
+
 		COM[_tcslen(COM) - 1] = '\0';
 
-		if (_tcscmp(COM, TEXT("pause"))==0) {
+		if (_tcscmp(COM, TEXT("pause")) == 0) {
 
 			if (game.state == 1)
 				game.state = 0;
@@ -567,16 +593,16 @@ int _tmain(int argc, LPTSTR argv[]) {
 				game.state = 1;
 
 		}
-		else if (_tcscmp(COM, TEXT("restart"))==0)
+		else if (_tcscmp(COM, TEXT("restart")) == 0)
 			game.state = 2;
-			
+
 
 
 		//_tprintf(TEXT("Estado: %d\n"), game.state);
 
-		
 
-	} while (_tcscmp(COM, TEXT("close"))!=0 && game.state != 3);
+
+	} while (_tcscmp(COM, TEXT("close")) != 0 && game.state != 3);
 
 	game.state = 3;
 
@@ -584,11 +610,11 @@ int _tmain(int argc, LPTSTR argv[]) {
 	tab.terminar = 1;
 
 	//esperar que a thread termine
-	WaitForMultipleObjects(2,hThread,FALSE, 2000);
-	WaitForMultipleObjects(game.nFaixas, ThreadChangeTab , FALSE, 2000);
+	WaitForMultipleObjects(2, hThread, FALSE, 2000);
+	WaitForMultipleObjects(game.nFaixas, ThreadChangeTab, FALSE, 2000);
 	UnmapViewOfFile(dados.memPar);
-	//CloseHandles ... mas Ã© feito automaticamente quando o processo termina
-	
+	//CloseHandles ... mas é feito automaticamente quando o processo termina
+
 
 	return 0;
 }
